30f0654336450bb0481e2dec93223ebc536b19af
Gayaek <gaya.ekstein@gmail.com>
2024-12-25 15:26:13 +0200

    first commit

---
+++ JS/ajaxcalls.js
@@ -0,0 +1,11 @@
+﻿function ajaxCall(method, api, data, successCB, errorCB) {
+    $.ajax({
+        type: method,
+        url: api,
+        data: data,
+        contentType: "application/json",
+        dataType: "json",
+        success: successCB,
+        error: errorCB
+    });
+}
