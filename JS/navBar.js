9062db193d8929a14ff2a14073853f1783cc27bd
Gayaek <gaya.ekstein@gmail.com>
2025-01-21 15:08:39 +0200

    almost final HW4

--- JS/navBar.js
+++ JS/navBar.js
@@ -6,10 +6,13 @@ function getUserOnNavBar(userData) {
     console.error("Navbar placeHolder NOT FOUND");
     return;
   }
+
+  const userName =
+    userData.email === "admin@admin.com" ? "BOSS" : userData.name;
   const detTemplate = ` 
             <div class="currentUser" "> 
               <div class="card-details"> 
-                <p id="card-current-name" class="card-text">Welcome ,  ${userData.name}!</p>
+                <p id="card-current-name" class="card-text">Welcome ,  ${userName}!</p>
               </div> 
             </div> `;
   userDetailsPlaceHolder.innerHTML = detTemplate;
